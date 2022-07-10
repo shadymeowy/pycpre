2257fb17a84d49cba4f85e96160b48b309a0781c
shadymeowy <tolgademirdal@gmail.com>
2022-07-10 15:25:54 +0300

    add --remove, --version and --no-import flags

--- pycpre/__main__.py
+++ pycpre/__main__.py
@@ -1,3 +1,4 @@
+from re import sub
 from pycpre import process_file
 import os
 import argparse
@@ -8,6 +9,9 @@ def main():
         prog='pycpre', description='pycpre is a prepressor replacement written in Python for the C preprocessor.')
     parser.add_argument('-n', '--no-run', action='store_true', help='disable running the processed file')
     parser.add_argument('-o', '--output', help='output file')
+    parser.add_argument('-r', '--remove', action='store_true', help='remove the .py files after running')
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.1.0')
+    parser.add_argument('-i', '--no-import', action='store_true', help='disable auto-importing pycpre')
     parser.add_argument('files', nargs='+', help='files to process')
     args = parser.parse_args()
     for file in args.files:
@@ -17,11 +21,13 @@ def main():
             if len(args.files) > 1:
                 raise Exception("Cannot specify output file when processing multiple files")
             output = args.output
-        process_file(file, output)
+        process_file(file, output, auto_import=not args.no_import)
 
     if not args.no_run:
         import subprocess, sys
         subprocess.call([sys.executable, output])
+        if args.remove:
+            subprocess.call(['rm', output])
 
 
 if __name__ == "__main__":
