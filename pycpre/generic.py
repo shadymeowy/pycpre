f7026619fda0d8c7d9688fe99b9767f694a92975
shadymeowy <tolgademirdal@gmail.com>
2022-07-14 03:31:00 +0300

    rework generics

--- pycpre/generic.py
+++ pycpre/generic.py
@@ -1,6 +1,3 @@
-from .special import cembed, cbuild, cdeps
-
-
 class Generic():
     def __init__(self, fun):
         self.fun = fun
@@ -18,30 +15,5 @@ class Generic():
             return self(args)
 
 
-class Namespace(dict):
-    __getattr__ = dict.__getitem__
-    __setattr__ = dict.__setitem__
-
-    def __hash__(self):
-        return hash(self.proxy)
-
-    def __cembed__(self):
-        return cembed(self.proxy)
-
-    def __cbuild__(self):
-        return cbuild(self.proxy)
-
-    def __cdeps__(self):
-        return cdeps(self.proxy)
-
-
 def generic(fun):
     return Generic(fun)
-
-
-def generic_space(fun):
-    def r(*args, **kargs):
-        S = Namespace()
-        S.proxy = fun(S, *args, **kargs)
-        return S
-    return Generic(r)
